0a531f3dc291708b38dc59b5ada4f59aa6b78f11
Steve Larsen <slarsen@afcorp.advancefinancialinc.com>
2021-04-29 08:23:25 -0500

    minor changes to display

--- src/components/PastListItems.js
+++ src/components/PastListItems.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_LAUNCH_DATA } from "../queries/getLaunchData";
